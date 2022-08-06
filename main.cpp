21e0dd95ac1a04f2aba42e32dae751ff408d1bbb
Kevin <kwaang@umich.edu>
2022-08-06 14:46:29 -0400

    correctly set up cmake and sdl2 libraries

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
-#include <iostream>
+#define SDL_MAIN_HANDLED
 
-int main() {
+#include <iostream>
+#include <SDL.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, const char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
+
+    //The window we'll be rendering to
+    SDL_Window* window = nullptr;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = nullptr;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    } else
+    {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == nullptr )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }  else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, nullptr, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Wait two seconds
+            SDL_Delay( 2000 );
+        }
+    }
+
+    //Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
     return 0;
 }
